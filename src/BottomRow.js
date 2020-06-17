c7ac95758c318decec1d2e3485bcc9642fb1dcbe
Adam Osburn <wizardofosburn@gmail.com>
2020-06-16 23:21:29 -0400

    fixed setters to not change constants, added onClick 'attributes'

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -11,19 +11,19 @@ const BottomRow = () => {
   return (
     <div className="bottomRow">
       <div className="down">
-        <h3 className="down__title" onClick={()=>{setDown(down++)}}>Down</h3>
+        <h3 className="down__title" onClick={()=>{setDown(down + 1)}}>Down</h3>
         <div className="down__value">{down}</div>
       </div>
       <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
+        <h3 className="toGo__title" onClick={()=>{setToGo(toGo - 1)}}>To Go</h3>
         <div className="toGo__value">{toGo}</div>
       </div>
       <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
+        <h3 className="ballOn__title" onClick={()=>{setBallOn(ballOn + 1)}}>Ball on</h3>
         <div className="ballOn__value">{ballOn}</div>
       </div>
       <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
+        <h3 className="quarter__title" onClick={()=>{setQuarter(quarter + 1)}}>Quarter</h3>
         <div className="quarter__value">{quarter}</div>
       </div>
     </div>
